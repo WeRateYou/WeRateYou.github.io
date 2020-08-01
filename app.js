53650ccb667dc10d9fccb89201f1e31d18170204
bernieem <berniemaecruz@gmail.com>
2020-08-01 16:13:20 +1200

    STYLE.css change

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ var table = document.getElementById("Rates").getElementsByTagName('tbody')[0];
 table.insertRow(2);
 table.rows[2].insertCell(0);
 table.rows[2].cells[0].innerHTML = "WORK PLeASE";
+
+JSON.parse(sampledata);
