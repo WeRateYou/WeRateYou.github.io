08aac34661f55ba93d12e463641f6d51d1154d0e
Beverley Sun <beverleysun0205@gmail.com>
2020-08-02 10:56:22 +1200

    Loading default AUD table data

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-var table = document.getElementById("rates").getElementsByTagName('tbody')[0];
-var row = table.insertRow(2);
-row.id="table-body-row";
-
-table.rows[2].insertCell(0);
-table.rows[2].cells[0].innerHTML = "WORK PLeASE";
-
-var data = JSON.parse(`{
-    "to": "jpy",
-    "from": "nzd",
-    "results": {
-        "asb": 85.0469,
-        "bnz": 71.28
-    }
-}`);
-
-
-table.rows[2].insertCell(1);
-table.rows[2].insertCell(2);
-table.rows[2].cells[1].innerHTML = data.results.asb;
-table.rows[2].cells[2].innerHTML = "Something";
+loadTableData("AUD", true, 1);
 
 let fromSelect = document.querySelector("#from");
 let toSelect = document.querySelector("#to");
