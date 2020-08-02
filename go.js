f8045b0c37e93796bc97aad61e43e720ec9c5580
DarkMatterMatt <mattmoran2000@gmail.com>
2020-08-02 13:33:10 +1200

    Remove useless statement

--- go.js
+++ go.js
@@ -6,7 +6,6 @@ button.addEventListener("click", go, false);
 function displayTime(){
     let time = new Date().toString();
     let footerText = "Data valid at " + time;
-    document.createElement("h5");
     footer.innerHTML = footerText;
 }
 
@@ -15,7 +14,7 @@ function go(){
     let amount = document.querySelector("#amount").value;
     let from = document.querySelector("#from").value;
     let to = document.querySelector("#to").value;
-    if (from === "NZD" && to !=="NZD"){
+    if (from === "NZD" && to !== "NZD"){
         loadTableData(to, false, amount);
     }else if (to === "NZD" && from !== "NZD"){
         loadTableData(from, true, amount);
