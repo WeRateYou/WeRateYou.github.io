57d3705e07548bbabaa874caccf7ee656ec81b7c
DarkMatterMatt <mattmoran2000@gmail.com>
2020-08-02 12:04:27 +1200

    Add currencies, update API url

--- loadTableData.js
+++ loadTableData.js
@@ -1,10 +1,24 @@
 const tbody = document.getElementById("rates-body");
+const toElem = document.getElementById("to");
+const fromElem = document.getElementById("from");
 
 let json = null;
 
-fetch("http://mattm.win:8080/api/v1/resources/rates/all").then(r => r.json()).then(json_ => {
+fetch("https://rate-selector.herokuapp.com/api/v1/resources/rates/all").then(r => r.json()).then(json_ => {
     json = json_;
-    loadTableData("AUD", false, 100);
+    loadTableData("AUD", false, 1);
+    const supportedCurrencies = [...new Set(Object.values(json).flatMap(v => Object.keys(v.rates)))];
+
+    let html = "";
+    supportedCurrencies.forEach(c => {
+        if (currencies[c] != null) {
+            html += `<option value="${c.toUpperCase()}">${c.toUpperCase()} (${currencies[c]})</option>`
+        }
+    });
+    
+    toElem.innerHTML = html;
+    fromElem.innerHTML = '<option value="NZD" selected="selected">NZD (New Zealand Dollar)</option>' + html;
+    toElem.value = "AUD";
 });
 
 function loadTableData(currency, bankIsBuying, youHave) {
@@ -13,15 +27,16 @@ function loadTableData(currency, bankIsBuying, youHave) {
         if (v.rates[currency.toLowerCase()] == null) {
             return null;
         }
-        const rate = v.rates[currency.toLowerCase()][bankIsBuying ? "buy" : "sell"];
+        const rate = v.rates[currency.toUpperCase()][bankIsBuying ? "buy" : "sell"];
         if (rate == null || rate === 0) {
             return null;
         }
+        const youGet = bankIsBuying ? youHave / rate : youHave * rate;
         return {
             id: id++,
             name: v.shortName,
             rate,
-            youGet: (rate * youHave).toFixed(2),
+            youGet: youGet.toFixed(2),
         }
     }).filter(x => x);
 
