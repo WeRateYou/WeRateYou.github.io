80138e5b5c82c08c4533adad942c10ed937f0ce0
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-08-04 16:57:04 +1200

    Complete merge of master

--- loadTableData.js
+++ loadTableData.js
@@ -66,44 +66,22 @@ function loadTableData(currency, bankIsBuying, youHave) {
 
 // display currrently supported providers
 function loadProviderCards(){
-    let filterSection = document.getElementsByClassName("sources")[0];
-    let tableSection = document.getElementsByClassName("sources")[1];
+    let tbodyWeb = document.getElementsByClassName("sources-table-body")[0];
+    let tbodyMobile = document.getElementsByClassName("sources-table-body")[1];
+    let content = "";
 
-    Object.entries(json.providers).forEach(([source, { logo }]) =>{
+    Object.values(json.providers).forEach(({ shortName, logo }) => {
         // Sources on side bar for web
-        sourceDiv1 = document.createElement("div");
-        sourceSpan1 = document.createElement("span");
-        sourceIMG1 = document.createElement("img");
-
-        sourceDiv1.className = "data-source";
-        sourceSpan1.className = "data-source-text";
-        sourceIMG1.className = "source-img";
-
-        sourceSpan1.innerHTML = source.toUpperCase();
-        sourceIMG1.src = logo;
-
-        // Sources under map for mobile
-        sourceDiv2 = document.createElement("div");
-        sourceSpan2 = document.createElement("span");
-        sourceIMG2 = document.createElement("img");
-
-        sourceDiv2.className = "data-source";
-        sourceSpan2.className = "data-source-text";
-        sourceIMG2.className = "source-img";
-
-        sourceSpan2.innerHTML = source.toUpperCase();
-        sourceIMG2.src = logo;
-
-        // Add elements to DOM
-        sourceDiv1.appendChild(sourceSpan1);
-        sourceDiv1.appendChild(sourceIMG1);
-
-        sourceDiv2.appendChild(sourceSpan2);
-        sourceDiv2.appendChild(sourceIMG2);
-        
-        filterSection.appendChild(sourceDiv1);
-        tableSection.appendChild(sourceDiv2);
+        tableRow = `
+            <tr class="table-body-row">
+                <td>${shortName} </td>
+                <td class="source-logo-cell"><img src="${logo}" class="source-logo"></td>
+            </tr>
+        `;
+        content += tableRow;
     });
+    tbodyWeb.innerHTML = content;
+    tbodyMobile.innerHTML = content;
 }
 
 // attempt to load live data, fallback to sample data
