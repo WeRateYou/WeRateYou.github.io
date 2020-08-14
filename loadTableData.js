1fd34fd04d6db1e0df0451aceef2aaababa533fe
Beverley Sun <beverleysun0205@gmail.com>
2020-08-14 23:10:18 +1200

    Loading page display to none when finished

--- loadTableData.js
+++ loadTableData.js
@@ -92,7 +92,7 @@ fetchJsonFromApi()
     .then(() => {
         loadTableData("AUD", false, 120);
         toElem.value = "AUD";
-        document.querySelector('.loading-wrap').style.zIndex = -1;
+        document.querySelector('.loading-wrap').style.display = "none";
     })
     .catch(() => {
         json = SAMPLE_DATA;
